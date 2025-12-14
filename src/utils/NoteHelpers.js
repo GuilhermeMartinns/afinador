414dca1c55becce2d4f6b2fe8ee300650a555440
Guilherme Martins <guilhermemartinns09@gmail.com>
2025-12-14 11:32:29 -0300

    Muda o símbolo de # para ♯

--- src/utils/NoteHelpers.js
+++ src/utils/NoteHelpers.js
@@ -1,4 +1,4 @@
-export const NOTE_NAMES =["C", "C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+export const NOTE_NAMES =["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"];
 export const NOTE_NAMES_FLAT =["C", "D♭","D","E♭","E","F","G♭","G","A♭","A","B♭","B"];
 export const getNoteDetails = (frequency, isFlatNote = false) => {
     
