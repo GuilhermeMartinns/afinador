349826c0ad5979024046d856f8a341ced8409aa7
Guilherme Martins <guilhermemartinns09@gmail.com>
2025-12-14 14:52:15 -0300

    Adiciona lógica para cortar início e fim do buffer, melhorando a precisão da detecção

--- src/utils/PitchDetector.js
+++ src/utils/PitchDetector.js
@@ -13,8 +13,31 @@ export const autoCorrelate = (buffer, sampleRate) => {
     //root mean square mede o volume do sinal
     //se o som for muito baixo ou silencio não tenta detectar a frequência
     const rootMeanSquare = Math.sqrt(sumOfSquares / SIZE);
-
+    // funciona como um noise gate
     if (rootMeanSquare < 0.01){
         return -1; //sinal muito fraco
+    }
+
+    // corta o ínicio e o fim do buffer para aumentar a precisão da detecção
+    // pois se cortar no meio do pico ou no meio de um vale pode gerar erros de calculo
+    // então tenta cortar mais próximo do repouso (perto de 0)
+    let r1 = 0;
+    let r2 = SIZE -1;
+    const threshold = 0.2;
+
+    // corta o inicio vazio
+    for (let i = 0; i < SIZE / 2; i++){
+        if (Math.abs(buffer[i]) < threshold){
+            r1 = i;
+            break;
+        }
+    }
+
+    // corta o final vazio
+    for (let i = 1; i < SIZE / 2; i++){
+        if (Math.abs(buffer[SIZE -i ] < threshold)){
+            r2 = SIZE - i;
+            break;
+        }
     }
 }
