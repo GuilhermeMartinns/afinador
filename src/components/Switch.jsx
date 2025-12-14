416030c31be530cf3da9a8a2d9a80dcc49e88d5e
Guilherme Martins <guilhermemartinns09@gmail.com>
2025-12-14 13:35:11 -0300

    Atualiza componente Switch para usar ícones de sustenido e bemol

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -2,36 +2,68 @@ import React from "react";
 
 const Switch = ({ isOn, handleToggle }) => {
   return (
-    <button
-      type="button"
-      role="switch"
-      aria-checked={isOn}
-      className={`
-        appearance-none 
-        relative 
-        w-14 h-8 
-        rounded-full 
-        p-1 
-        cursor-pointer
-        transition-colors duration-300 ease-in-out
-        focus:outline-none 
-        ${isOn ? 'bg-green-500' : 'bg-gray-600'}
-      `}
+    
+    <div
+      onClick={handleToggle}
+      style={{
+        width: '38px',           
+        height: '20px',         
+        backgroundColor: isOn ? '#22c55e' : '#4b5563', 
+        borderRadius: '9999px',  
+        position: 'relative',    
+        cursor: 'pointer',
+        transition: 'background-color 0.3s',
+        display: 'flex',         
+        flexShrink: 0,            
+        alignItems: 'center',
+        justifyContent:'space-between',
+        boxSizing: 'border-box'
+      }}
     >
-      <span
-        className={`
-          absolute 
-          top-1 
-          left-1 
-          bg-white 
-          w-6 h-6 
-          rounded-full 
-          shadow-md 
-          transition-transform duration-300 ease-out
-          ${isOn ? 'translate-x-6' : 'translate-x-0'}
-        `}
-      />
-    </button>
+        {/*Icone de fundo Sustenido */}
+        <span style={{
+            color: 'white',
+            fontWeight: 'bolder',
+            fontSize: '11px',
+            opacity: isOn ? 1 : 0,
+            transition: 'opacity 0.3s',
+            marginLeft: '8px'
+        }}>♯</span>
+
+        {/* Icone de fundo Bemol */}
+        <span style={{
+            color: 'white',
+            fontWeight: 900,
+            fontSize: '11px',
+            opacity: isOn ? 0 : 1,
+            transition: 'opacity 0.3s',
+            marginRight: '8px'
+        }}>♭</span>
+      {/* BOLINHA */}
+      <div
+        style={{
+          width: '15px',         
+          height: '15px',        
+          backgroundColor: 'white',
+          borderRadius: '50%',   
+          position: 'absolute',  
+          top: '2px',           
+          left: '3px',           
+          transition: 'transform 0.3s',
+          transform: isOn ? 'translateX(18px)' : 'translateX(0)',
+          boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: isOn ? '#22c55e' : '#4b5563',
+          fontWeight: 'bolder',
+          fontSize: '12px',
+          zIndex: 10
+        }}
+      >
+        {isOn? '♭' : '♯'}
+        </div>
+    </div>
   );
 };
 
