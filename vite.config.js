cc2286ff9d133ff14b5129e47b5a66beda0aad0a
Guilherme Martins <guilhermemartinns09@gmail.com>
2025-12-16 23:11:31 -0300

    Adiciona dependencias do plugin de pwa do vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { VitePWA } from 'vite-plugin-pwa' // <-- Importe aqui
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icons/*.png'], // Os arquivos que devem ser cacheados
+      manifest: {
+        name: 'Afinador Cromático',
+        short_name: 'Afinador',
+        description: 'Seu afinador cromático simples e rápido.',
+        theme_color: '#030612',
+        background_color: '#000000',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   server: {
-    host: true,
+    host: true, // Para acesso via IP
     port: 5173,
-    
   }
 })
